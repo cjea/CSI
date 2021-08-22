f68261bdda7631a1fd7358f9141a8f265ce5d0ac
CJ Apel <cjapel@liveramp.com>
2021-08-21 17:31:20 -0700

    Parse opts

--- prep/c/ls.c
+++ prep/c/ls.c
@@ -10,13 +10,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
-#define NUM_COLUMNS 4
+#define MAX_NUM_COLUMNS 10
+
+void usage(int ec) {
+  printf(
+      "Usage: ls [ -s ] [ -m ] [ -u ] [ -g ] dir\n-s\tfilesize\n-m\tpermission "
+      "mode\n-u\tuser owner\n-g\tgroup owner\n");
+  exit(ec);
+}
 
 int main(int argc, char *argv[]) {
-  char *dir_name = argv[1];
+  row_builder *fptr[MAX_NUM_COLUMNS] = {};
+  int c;
+  int num_columns = 0;
+  while ((c = getopt(argc, argv, "msugh")) != -1) {
+    switch (c) {
+    case 'h':
+      usage(0);
+      break;
+    case 'm':
+      fptr[num_columns++] = &full_mode;
+      break;
+    case 's':
+      fptr[num_columns++] = &filesize;
+      break;
+    case 'u':
+      fptr[num_columns++] = &username;
+      break;
+    case 'g':
+      fptr[num_columns++] = &groupname;
+      break;
+    }
+  }
+  if (argc < 2) {
+    printf("must specify a directory\n");
+    usage(1);
+  }
+  char *dir_name = argv[argc - 1];
   if (dir_name[strlen(dir_name) - 1] != '/') {
-    printf("input must be a directory (end with a slash)\n");
+    printf("input directory must end with a slash\n");
     exit(1);
   }
   DIR *dirp = opendir(dir_name);
@@ -26,14 +60,12 @@ int main(int argc, char *argv[]) {
   }
 
   struct dirent **dirents = dirents_in_dir(dirp);
-  row_builder *fptr[NUM_COLUMNS] = {&full_mode, &username, &groupname,
-                                    &filesize};
 
   for (int i = 0; dirents[i] != NULL; i++) {
     char *basename = dirents[i]->d_name;
     if (strlen(basename) > 0) {
       char *abs_path = strcat_i(dir_name, basename);
-      format_and_print_row(NUM_COLUMNS, fptr, abs_path);
+      format_and_print_row(num_columns, fptr, abs_path);
       free(abs_path);
     }
   }
