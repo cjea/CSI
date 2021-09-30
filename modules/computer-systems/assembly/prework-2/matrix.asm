0248f737f3b1026bef40413a45fa8d4d446eee36
CJ <cjeapel@gmail.com>
2021-09-29 22:57:57 -0700

    matrix.asm

--- modules/computer-systems/assembly/prework-2/matrix.asm
+++ modules/computer-systems/assembly/prework-2/matrix.asm
@@ -1,9 +1,25 @@
 section .text
 global index
-index:
-	; rdi: matrix
-	; rsi: rows
-	; rdx: cols
-	; rcx: rindex
-	; r8: cindex
-	ret
+
+; rdi: matrix
+; rsi: rows
+; rdx: cols
+; rcx: rindex
+; r8: cindex
+;;
+;; element    = matrix + (int * cols * rindex) + (cindex * int)
+;;              where int = 4 
+
+index:  
+  imul    rcx, INT_SIZE
+  imul    rdx, rcx
+
+  imul    r8, INT_SIZE
+  add     rdx, r8
+
+  add     rdi, rdx
+  mov     rax, [rdi]
+  ret
+
+section .data
+INT_SIZE: equ 4
