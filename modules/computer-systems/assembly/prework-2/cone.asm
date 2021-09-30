2af5483c63984ab9c37782de17a86b16799c1fce
CJ <cjeapel@gmail.com>
2021-09-29 23:50:18 -0700

    cone.asm

--- modules/computer-systems/assembly/prework-2/cone.asm
+++ modules/computer-systems/assembly/prework-2/cone.asm
@@ -2,5 +2,13 @@ default rel
 
 section .text
 global volume
-volume:
+volume: ; (PI * (r^2) * h) / 3
+  mulss xmm0, xmm0
+  mulss xmm0, [pi]
+  mulss xmm0, xmm1
+  divss xmm0, [f_three]
  	ret
+  
+section .rodata
+pi:       dd 3.14159
+f_three:  dd 3.0
